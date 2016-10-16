8779a4ec70e85504ee3d3b112f1fb57ce387705f
JayJayEvans <jason_evans2@baylor.edu>
2016-10-15 23:57:54 -0500

    Account page will bring up name and email if user is logged in

--- src/main/resources/public/account/account.controller.js
+++ src/main/resources/public/account/account.controller.js
@@ -1,12 +1,30 @@
 (function() {
   'use strict';
 
-    angular.module('app')
-      .controller('accountController', accountController);
+  angular
+      .module('app')
+      .controller('accountController',accountController);
 
-    accountController.$inject = [];
-    function accountController() {
-      var vm = this;
-      vm.message = 'User Account';
+  accountController.$inject = ['myAccount', '$cookies'];
+
+  function accountController( myAccount, $cookies) {
+    var vm = this;
+    vm.title = 'Account';
+
+    vm.account = function account() {
+
+      var success = function(response) {
+        document.getElementById("accName").innerHTML = 'Name: ' + response.data.name;
+        document.getElementById("accEmail").innerHTML = 'Email: ' + response.data.email;
+
+      };
+
+      var error = function(response) {
+        console.error('Failed');
+      };
+
+      return myAccount.fetchUser().then(success, error);
     }
+  }
+
 })();
---
+++ src/main/resources/public/account/account.service.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Jason on 10/15/2016.
+ */
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .service('myAccount', myAccount);
+
+    myAccount.$inject = ['$http', '$cookies'];
+
+    function myAccount($http, $cookies) {
+        var value = $cookies.get('userObj');
+        //value.replace("{\"token\":\"", "Bearer ");
+        value = value.substring(10, value.length - 2);
+        var exports = {
+            fetchUser: fetchUser
+        };
+
+        return exports;
+
+
+        function fetchUser() {
+            var userRequest = {
+                method: 'GET',
+                url: 'https://podsurfer-4.herokuapp.com/api/user/me',
+                headers: {
+
+                    Authorization: 'Bearer ' + value
+                }
+
+            }
+            return $http(userRequest);
+        }
+    }
+})();
