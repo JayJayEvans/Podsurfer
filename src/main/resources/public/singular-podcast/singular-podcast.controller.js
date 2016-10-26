bc780228b0afad5e1203c5c5a7896a2db1f2e2f0
Hank Harrison <Hank Harrison>
2016-10-25 20:12:32 -0500

    Commit of the singular podcast files

--- src/main/resources/public/singular-podcast/singular-podcast.controller.js
+++ src/main/resources/public/singular-podcast/singular-podcast.controller.js
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
-=======
 /**
  *Created by Hank Harrison on 10/18/2016
  * Update History:
  *
  */
-/*
->>>>>>> ff5d5cbc4f0bced999a7edfd501de867c604dc8c
+
 (function() {
     'use strict';
 
@@ -37,4 +34,4 @@
         }
     }
 
-    })();*/
+    })();
