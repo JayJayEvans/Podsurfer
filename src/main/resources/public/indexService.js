eca635abf50440adc45e32016f809fc61ad01c56
JayJayEvans <jason_evans2@baylor.edu>
2016-11-28 18:36:29 -0600

    Account is now login and account

---
+++ src/main/resources/public/indexService.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Jason on 11/28/2016.
+ */
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .service('indexService', indexService);
+
+    indexService.$inject = ['$http', '$cookies'];
+
+    function indexService($http, $cookies) {
+
+        var token = $cookies.get('userObj');
+        if(token != null)
+            token = token.substring(10, token.length - 2);
+
+        var exports = {
+            getUserInfo: getUserInfo
+
+        };
+
+        return exports;
+
+
+        function getUserInfo() {
+
+            var apiRequest = {
+                method: 'GET',
+                url: 'https://podsurfer-4.herokuapp.com/api/user/me',
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            }
+
+            return $http(apiRequest);
+        }
+
+
+
+
+    }
+})();
