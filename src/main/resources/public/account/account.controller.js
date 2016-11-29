fe4917bce9b6641bb09117dd4743c8a055e7d271
Tim Arterbury <timarterbury@gmail.com>
2016-11-29 15:53:03 -0600

    Adds functionality for user to delete any review on their account page. Also updates consistency of the addPodcast controller and fixes url problems. NEED TO FIX EPISODES AND TAGS also we need to throw an error when user types in letters into the release or length inputs.

--- src/main/resources/public/account/account.controller.js
+++ src/main/resources/public/account/account.controller.js
@@ -17,9 +17,12 @@
     vm.interests = [];
     vm.bookmarks = [];
     var stolen = vm.interests;
+    vm.reviews = [];
+    
+    
     vm.account = function account() {
 
-      var success = function(response) {
+      var userInfoSuccess = function(response) {
         
         vm._id = response.data._id;
         vm.name = response.data.name;
@@ -30,9 +33,9 @@
         stolen = vm.interests;
        // $cookies.putObject('userId',response.data._id);
 
-      }
+      };
 
-      var error = function(response) {
+      var userInfoError = function(response) {
 
         console.error('Failed');
         console.error($cookies.get('userObj'))
@@ -42,10 +45,46 @@
         document.getElementById("myAnchor").innerHTML = "User Is Not Logged In";
         location.href = '#/login';
         //alert('You must be signed in to view your profile');
-      }
-      return accountService.getUserInfo().then(success, error);
+      };
+      
+      
+      var reviewSuccess = function(response) {
+        console.log("Sucessfully grabbed reviews.");
+        vm.reviews = response.data;
+      };
+      
+      var reviewError = function(response) {
+        
+        console.error('Failed grabbing user reviews');
+        console.log(response.data);        
+        
+      };
+      
+      accountService.getUserInfo().then(userInfoSuccess, userInfoError);
+      accountService.getUserReviews().then(reviewSuccess, reviewError);
+      return;
 
     };
+    
+    vm.deleteReview = function deleteReview(reviewId) {
+      
+      var success = function(response) {
+        console.log("Sucessfully deleted review");
+        // Reload vm.account
+        vm.account();
+      };
+      
+      var error = function(response) {
+        console.log("Could not delete review");
+        console.log(response.data); 
+      };
+      
+      
+      accountService.deleteReview(reviewId).then(success, error);
+      return;
+    };
+    
+    
     
     vm.addInterest = function addInterest(interest) {
       
@@ -70,8 +109,6 @@
         
         return;       
 
-      
-      
     };
 
 
